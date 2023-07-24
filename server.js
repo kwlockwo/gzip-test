7d73cbebc0a7216489ca34f331d07010ee3f0cd2
Keith Lockwood <kwlockwo@gmail.com>
2023-07-24 10:40:30 +1000

    add post

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const compression = require('compression');
-
 const app = express();
 
 const port = process.env.PORT || 5050
 
+app.use(express.json());
+
 app.get('/compressed', compression(), function(req, res) {
-    res.send('Hello World'.repeat(50000));
+    res.send('Hello World '.repeat(50000));
+});
+
+app.post('/compressed', compression(), function(req, res) {
+    let data = req.body
+    res.send(`Hello ${data.name} `.repeat(50000));
 });
 
 app.get('/notcompressed', function(req, res) {
